70b28fcb544449fb44ae443e1734a61ab1d89b22
jbdouet <jean.baptiste.douet@gmail.com>
2017-12-21 10:58:46 +0100

    dtg

--- TP6_expressions.R
+++ TP6_expressions.R
@@ -27,7 +27,7 @@ data_expressions.train<-data_expressions[train,]
 ############################### Models ############################### 
 
 
-#################### SVM #################### 
+#################### SVM linear #################### 
 #data_expressions$y <- as.factor(as.integer(data_expressions))
 
 n_folds <- 10
@@ -41,9 +41,9 @@ for (k in 1:n_folds) {# we loop on the number of folds, to build k models
   train_xy <- data_expressions[-test_i, ]
   test_xy <- data_expressions[test_i, ]
   print(k)
-  model_svmLinear <- train(train_xy[,1:4200],train_xy$y,method='mlp',trControl= trainControl(
+  model_svmLinear <- train(train_xy[,1:4200],train_xy$y,method='svmLinear',trControl= trainControl(
     method = "cv",
-    number =2,
+    number =10,
     verboseIter = TRUE))
   predictions_svmLinear<-predict.train(object=model_svmLinear,test_xy[,1:4200])
   cf<-confusionMatrix(predictions_svmLinear,test_xy$y) 
@@ -52,9 +52,52 @@ for (k in 1:n_folds) {# we loop on the number of folds, to build k models
 CVerror= sum(CV)/length(CV)
 CV
 CVerror
-
+cf
 test_xy$y
 predictions_svmLinear
 I<-matrix(as.matrix(test_xy[4,1:4200]),60,70)
 I1 <- apply(I, 1, rev)
 image(t(I1),col=gray(0:255 / 255))
+
+
+#################### SVM radial #################### 
+#data_expressions$y <- as.factor(as.integer(data_expressions))
+
+n_folds <- 1
+folds_i <- sample(rep(1:n_folds, length.out = 36)) # !!! le ntrain doit correspondre à la taille du dataset que l'on utilisera dans la boucle de cross validation 
+table(folds_i) # Pas le même nombre d'éléments 
+CV<-rep(0,n_folds)
+for (k in 1:n_folds) {# we loop on the number of folds, to build k models
+  test_i <- which(folds_i == k)
+  # les datasets entre le fit et le predict doivent être les mêmes car c'est le même dataset que l'on divise en k-fold 
+  # on peut utiliser le data set complet ou seulement le train et avoir une idée finale de la performance sur le test
+  train_xy <- data_expressions[-test_i, ]
+  test_xy <- data_expressions[test_i, ]
+  print(k)
+  model_svmRadial <- train(train_xy[,1:4200],train_xy$y,method='rf',trControl= trainControl(
+    method = "cv",
+    number =10,
+    verboseIter = TRUE))
+  predictions_svmRadial<-predict.train(object=model_svmRadial,test_xy[,1:4200])
+  cf<-confusionMatrix(predictions_svmRadial,test_xy$y) 
+  CV[k]<- cf$overall["Accuracy"]
+}
+CVerror= sum(CV)/length(CV)
+CV
+CVerror
+dim(train_xy)
+dim(test_xy)
+dim(data_expressions)
+test_xy$y
+predictions_svmRadial
+I<-matrix(as.matrix(test_xy[4,1:4200]),60,70)
+I1 <- apply(I, 1, rev)
+image(t(I1),col=gray(0:255 / 255))
+
+
+
+
+
+
+
+################## XGBOOST ################## 
