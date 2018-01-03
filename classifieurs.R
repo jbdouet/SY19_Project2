d7d79cc377b38654176f30ff391b34126f09f8d2
LouisMartignoni <martignonilouis@gmail.com>
2018-01-03 17:44:38 +0100

    classifieurs

--- classifieurs.R
+++ classifieurs.R
@@ -8,17 +8,15 @@ classifieur_expressions <- function(dataset) {
 }
 classifieur_characters <- function(dataset) {
   # Chargement de l’environnement
-  load("env.Rdata")
-  # Mon algorithme qui renvoie les prédictions sur le jeu de données
-  # ‘dataset‘ fourni en argument.
-  # ...
+  require(randomForest)
+  load("env.Rdata",.GlobalEnv)
+  predictions<- predict(.GlobalEnv$classifieur_character, dataset)
   return(predictions)
 }
 classifieur_parole <- function(dataset) {
   # Chargement de l’environnement
-  load("env.Rdata")
-  # Mon algorithme qui renvoie les prédictions sur le jeu de données
-  # ‘dataset‘ fourni en argument.
-  # ...
+  require('e1071')
+  load("env.Rdata",.GlobalEnv)
+  predictions<- predict(.GlobalEnv$classifieur_parole, dataset)
   return(predictions)
 }
